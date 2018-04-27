327beca8fcf5445ae403d783b98ab386b0f0d89f
Milena Atanasova <atamasowa40@gmail.com>
2018-04-27 11:12:13 +0300

    Completed the validation, added passwords comparing

--- src/app/auth/register.component.ts
+++ src/app/auth/register.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
-import { User } from '../models/user';
-import { AuthService } from '../core/auth.service';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { ReactiveFormsModule, FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../core/auth.service';
+import { PasswordValidation } from '../core/validate-passwords.service';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
   private registerForm: FormGroup;
@@ -15,22 +16,20 @@ export class RegisterComponent implements OnInit {
   private email: AbstractControl;
   private password: AbstractControl;
   private password2: AbstractControl;
-  private customValidator: Array<Validators>;
-  private errors: string
+  private comparedPasswords: string = '';
+  private errors: string;
 
   constructor(private authService: AuthService, private formBuilder: FormBuilder) { }
 
-  ngOnInit() {
-    this.customValidator = [
-      Validators.minLength
-    ];
-
+  public ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(1024)]],
+      username: ['', [Validators.required, Validators.minLength(+'6'), Validators.maxLength(+'1024')]],
       email: ['', [Validators.email, Validators.required]],
-      password: ['', [Validators.minLength(6), Validators.maxLength(256), Validators.required]],
-      password2: ['', [Validators.minLength(6), Validators.required]],
-    });
+      password: ['', [Validators.minLength(+'6'), Validators.maxLength(+'256'), Validators.required]],
+      password2: ['', [Validators.minLength(+'6'), Validators.required]],
+    }, {
+        Validators: PasswordValidation.MatchPasswords,
+      });
 
     this.username = this.registerForm.get('username');
     this.email = this.registerForm.get('email');
@@ -38,14 +37,14 @@ export class RegisterComponent implements OnInit {
     this.password2 = this.registerForm.get('password2');
   }
 
-  private register() {
+  private register(): void {
     const newUser = {
       username: this.registerForm.value.username,
       email: this.registerForm.value.email,
       password: this.registerForm.value.password,
     };
 
-    console.log(newUser)
+    console.log(newUser);
     this.authService.register(newUser).subscribe(
       (x) => console.log(x),
       (err: HttpErrorResponse) => this.errors = err.error.err);
@@ -65,5 +64,13 @@ export class RegisterComponent implements OnInit {
         return `Your ${fieldName} must be at least ${fieldLength} symbols!`;
       }
     }
+  }
+
+  private comparePasswords(): string {
+    if (!PasswordValidation.MatchPasswords(this.registerForm)) {
+      return `Passwords doesn't match`;
+    }
+
+    return '';
   }
 }
