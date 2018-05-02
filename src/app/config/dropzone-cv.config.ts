b98eea63150b504e609c2cc9d9cd5521ed608066
Targov <alextargov@gmail.com>
2018-05-02 18:19:11 +0300

    Changed the applications request urls

--- src/app/config/dropzone-cv.config.ts
+++ src/app/config/dropzone-cv.config.ts
@@ -9,7 +9,7 @@ export class DropzoneCvConfig {
     public readonly MAX_FILE_SIZE = 16;
 
     constructor() {
-        this.url = 'http://localhost:3012/api/jobs/upload-cv';
+        this.url = 'http://localhost:3012/api/applications/upload-cv';
         this.method = 'POST';
         this.autoProcessQueue = true;
         this.acceptedFiles = '.pdf, .doc, .docx';
