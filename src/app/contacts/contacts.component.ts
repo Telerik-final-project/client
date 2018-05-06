466fafcd5fd79542fa5c44b0ffbdea12fa6d01fb
Milena Atanasova <atamasowa40@gmail.com>
2018-05-07 01:39:26 +0300

    Added setup for google maps integration

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,15 +1,24 @@
+import { AgmCoreModule } from '@agm/core';
 import { Component, OnInit } from '@angular/core';
-
+import { IContact } from '../admin/contacts/_interfaces/contact.interface';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent implements OnInit, IContact {
+  public name: string;
+  public address: string;
+  public status: number;
+  public longtitude?: number;
+  public latitude?: number;
+
+  public lat: number = 51.678418;
+  public lng: number = 7.809007;
 
   constructor() { }
 
-  ngOnInit() {
+  public ngOnInit() {
   }
 
 }
